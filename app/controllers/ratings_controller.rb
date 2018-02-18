bfe30e2db85e58b4674f3658569368fe9faff595
Parker <parkermatheson@gmail.com>
2018-02-18 15:13:39 -0700

    Moved the form from the 'index' page to it's own 'new' page. Started setting up relationships between the form and the database. Started setting up the form to use ERB so that data collected in the form is useful.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,8 @@
 class RatingsController < ApplicationController
   def index
   end
+
+  def new
+    @review = Review.new
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,8 @@
 class Rating < ApplicationRecord
+  validates_presence_of :rating
+
+  # belongs_to :movie
+  # belongs_to :user, dependent: :destroy
+
+
 end
